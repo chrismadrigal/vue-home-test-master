9e85540228e3edee63989832a7813d132aa56143
Chris Madrigal <chris@rightforge.com>
2022-11-10 12:47:44 -0800

    Add main products component and fetch product.
    Consume Product component on Product view.
    Update product view to use  <suspense>

---
+++ src/components/Products.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container products-header">
+        <h2>Refurbished</h2>
+        ProductsSort here...
+    </div>
+    <div class="container" v-if="products">
+        ProductsGrid here.. {{ products }}
+    </div>
+</template>
+
+<script setup>
+import useApi from "@/composables/useApi.js";
+
+// Fetch the initial products
+const { response: products } = await useApi("/products");
+</script>
+
+<style scoped>
+.products-header {
+    margin-bottom: var(--sections-spacing-mobile);
+}
+
+@media (min-width: 768px) {
+    .products-header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+}
+</style>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,5 +1,19 @@
 <template>
-  <h1>Products here...</h1>
+  <suspense>
+    <Products />
+  </suspense>
+
 </template>
 
+<!--
+  This is simply a wrapper component to use suspense and  provide fallback content.
+  We are not displaying a #fallback template at the moment.
+ -->
+<script setup>
+import { onErrorCaptured } from "vue";
+import Products from "@/components/Products";
 
+onErrorCaptured(() => {
+  console.log("Sorry, something when wrong :(")
+});
+</script>
