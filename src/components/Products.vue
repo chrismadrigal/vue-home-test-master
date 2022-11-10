7c694e1a2bbbb2220d4e47f8952351e8b66d9fe2
Chris Madrigal <chris@rightforge.com>
2022-11-10 13:34:46 -0800

    Add ProductSort Component. Reload product when sort change.

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container products-header">
         <h2>Refurbished</h2>
-        <h3>ProductsSort here...</h3>
+        <ProductsSort @changeSort="onChangeSort" />
     </div>
     <div class="container" v-if="products">
         <ProductsGrid :products="products" />
@@ -9,12 +9,41 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
 import useApi from "@/composables/useApi.js";
+import ProductsSort from "@/components/ProductsSort";
 import ProductsGrid from "@/components/ProductsGrid";
 
 // Fetch the initial products
-const { response: products } = await useApi("/products");
+const { response: products, reload } = await useApi("/products");
 
+// Sort products ref
+const productsSort = ref("");
+
+// Event fire by ProductsSort component when changing sort
+const onChangeSort = (newSort) => {
+    productsSort.value = newSort;
+    sortProductsByPrice();
+};
+
+// Fetch new products sort by price or revert to default product sort.
+const sortProductsByPrice = () => {
+    let sortOrder = "";
+
+    if (productsSort.value === "priceAsc") {
+        sortOrder = "asc";
+    }
+
+    if (productsSort.value === "priceDesc") {
+        sortOrder = "desc";
+    }
+
+    const apiPath = sortOrder
+        ? `/products?_sort=priceCents&_order=${sortOrder}`
+        : "/products";
+
+    reload(apiPath);
+};
 </script>
 
 <style scoped>
---
+++ src/components/ProductsSort.vue
@@ -0,0 +1,53 @@
+<!--
+  We can use a custom select component here but for now
+  I'm using a native <select> element.
+-->
+<template>
+  <div class="select">
+    <select v-model="sort" @change="onChange">
+      <option value="">Sort By</option>
+      <option value="priceAsc">Price: Low - High</option>
+      <option value="priceDesc">Price: High - Low</option>
+    </select>
+  </div>
+</template>
+
+<script setup>
+import { ref, defineEmits } from "vue";
+const sort = ref("");
+
+// Emit an event after change.
+const emit = defineEmits(["changeSort"]);
+
+const onChange = () => {
+  emit("changeSort", sort.value);
+};
+</script>
+
+<style scoped>
+.select {
+  align-items: center;
+  position: relative;
+  min-width: 15ch;
+  max-width: 30ch;
+  border: 1px solid var(--color-border);
+  font-size: 1rem;
+  cursor: pointer;
+  background: #fff;
+}
+
+.select::after {
+  position: absolute;
+  top: 15px;
+  right: 12px;
+  color: var(--color-text-dark);
+  border-style: solid;
+  border-width: 0.17rem 0.17rem 0 0;
+  content: '';
+  display: inline-block;
+  height: 8px;
+  width: 8px;
+  vertical-align: top;
+  transform: rotate(135deg);
+}
+</style>
